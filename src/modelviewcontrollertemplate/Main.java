ad313ecba7e572d555c5c044bc4f033222b5e331
Mike Schoonover <mike@skoonie.com>
2015-02-24 17:50:12 -0700

    Changed code per IDE suggestion.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- src/modelviewcontrollertemplate/Main.java
+++ src/modelviewcontrollertemplate/Main.java
@@ -72,10 +72,9 @@ public static void main(String[] args)
     //Schedule a job for the event-dispatching thread:
     //creating and showing this application's GUI.
 
-    javax.swing.SwingUtilities.invokeLater(
-        new Runnable() {
-            @Override
-            public void run() { createController(); } });
+    javax.swing.SwingUtilities.invokeLater(() -> {
+        createController();
+    });
 
 }//end of Main::main
 //-----------------------------------------------------------------------------
